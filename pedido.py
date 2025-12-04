d2825ac8acfb2e4952e3f4e0536ee1703b224ff0
kete <ketelen.lima10@gmail.com>
2025-12-04 06:39:04 -0300

    novo

--- pedido.py
+++ pedido.py
@@ -1,26 +1,41 @@
-pedido=[]
-def novo_pedido(cardapio):
-    id=int(input("digite ID do item: "))
-    qtd=int(input("digite a quantidade: "))
-    item=cardapio[id]
+contador=1
+def novo_perdido(cardapio):
+     pedido={
+     "id": contador,
+     "itens": [],
+     "total": 0, 
+     }
+     contador+=1
+     print("adicione o item ao seu pedido")
 
-    for item in cardapio:
-        if item["id"]==id:
-            total=item["preço"] * qtd 
-
-        pedido.append({
-            "item":item["nome"],
-            "qtd": qtd,
-            "total": total
-        })
-    print("Seu pedido foi adicionado!")
-
-    print("ID nao foi encontrado")
-    return
-
+     while True:
+          pedido=[]
+          id_item=int(input("digite um id: "))
+          qtd=int(input("digite a quantidade: "))
+          for c in cardapio:
+               if c["id"]==id_item:
+                    pedido["itens"].append({
+                         'id':id_item,
+                         'quantidade':qtd
+                    })
+                    pedido["total"]+=item["preço"]* qtd
+                    return pedido
 def calcular_total(pedido,cardapio):
-    total_geral=sum([item["total"] for item in pedido])
-    if total_geral > 50:
-        divisao=total_geral/10
-        total=total_geral-divisao
+     total=pedido["total"]
+     if total> 50:
+          total*=0.9
+          print("a desconto")
+     else:
+          print("nao a desconto")
+
+quantidade_total=sum(item["quantidd"])               
+
+     
+               
+                    
+                    
+
+
+
+
         
